10e4eaa646b1b63cc860c40894873f40f3f84c65
Vaibtan <vaibhav21296@iiitd.ac.in>
2024-06-16 22:29:00 +0530

    home_commit

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -30,6 +30,23 @@ const Stairs = () => {
      CREATING A STAGGERED EFFECT W/ DECREASING DELAY FOR EACH SUBSEQUENT STEP.
 
         */ }
+            {[...Array(6)].map((_, index) => {
+                return (
+                    <motion.div 
+                        key = {index} 
+                        variants = {stairAnimation} 
+                        initial = "initial"
+                        animate = "animate"
+                        exit = "exit"
+                        transition = {{
+                            duration: 0.4,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1,
+                        }}
+                        className = "h-full w-full bg-white relative"
+                    />
+                );
+            })}
         </>
     );
 };
